fb5487aa95303908fd89613863f5cd62c43cafef
Darren Davie <darrendavie@yahoo.com.au>
2017-03-15 08:45:37 +1100

    Adjusted Application class to print out attacktypes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,5 @@
+import enums.AttackTypes;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,33 +8,22 @@ import static enums.Pokecards.MOLTEN;
 
 public class Application {
     public static void main(String[] args){
-        List<Pokecard>  pokecards =  new ArrayList<>();
-//        List<Attack>  attackList = new ArrayList<>();
-//
-//        attackList.add(new Attack(AttackTypes.TACKLE));
-//        attackList.add(new Attack(AttackTypes.LAVA_BALL));
-//        pokecards.add( new Pokecard("Moltan", 50, 0.7, attackList, Type.LAVA, EvolutionStatus.BASIC));
-//
-//        attackList = new ArrayList<>();
-//        attackList.add(new Attack(AttackTypes.DREAM_EATER));
-//        attackList.add(new Attack(AttackTypes.DREAM_BLAST));
-//        pokecards.add( new Pokecard("Dream Morning", 150, 2.0, attackList, Type.DREAM, EvolutionStatus.BASIC));
-
+        List<Pokecard>  pokecards =  new ArrayList<Pokecard>(){};
         pokecards.add( new Pokecard(MOLTEN));
         pokecards.add( new Pokecard(DREAM_MORNING));
-
 
         for(Pokecard pokecard: pokecards){
             System.out.println("Pokemon Name: " + pokecard.getName());
             System.out.println(" Hit Points: " + pokecard.getHitPoints());
             System.out.println(" Height: " + pokecard.getHeight());
-            System.out.println("TODO printout attack types");
-//            for ( Attack attack :pokecard.getAttacks()) {
-//                System.out.println(" Attack List " + attack.getName() + " " + attack.getDamage());
-//            }
+            for ( AttackTypes attack :pokecard.getAttacks()) {
+                System.out.println(" Attack List " + attack.getName() + " " + attack.getDamage());
+            }
             System.out.println(" enums.Type: " + pokecard.getType());
             System.out.println(" Evolution Status: " + pokecard.getEvolutionStatus());
         }
+
+
     }
 
 }
