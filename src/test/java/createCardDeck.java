1b30a414c24985286d141bc6ef13b2e3f9543bf4
Darren Davie <darrendavie@yahoo.com.au>
2017-03-16 17:49:07 +1100

    Refactored createCardDeck test

--- src/test/java/createCardDeck.java
+++ src/test/java/createCardDeck.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -5,27 +6,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
 
 public class createCardDeck {
+    PokecardDeckFactory  pdf;
+    PokeCardDeck deck;
+
+    @Before
+    public void setup(){
+        pdf = new PokecardDeckFactory();
+        deck = pdf.createCards();
+    }
 
     @Test
     public void createPokeDeckFacactory(){
-        PokecardDeckFactory pdf = new PokecardDeckFactory();
         assertThat(pdf,notNullValue());
     }
 
     @Test
     public void createPokeDeck( ){
-        PokecardDeckFactory pdf = new PokecardDeckFactory();
-        PokeCardDeck deck = pdf.createCards();
         assertThat(deck.getCardCount(), is(4));
     }
 
     @Test
     public void displayCards(){
-        PokecardDeckFactory pdf = new PokecardDeckFactory();
-        PokeCardDeck deck = pdf.createCards();
         deck.displayCards();
     }
 
+    @Test
+    public void dealCards(){
+
+    }
 
 }
 
